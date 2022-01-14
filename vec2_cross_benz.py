f8f8e1ee83d810f5c38bac5ac909dc37db0001ee
Micrafast <1191069521@qq.com>
2022-01-14 22:14:25 +0800

    Updated tools

--- vec2_cross_benz.py
+++ vec2_cross_benz.py
@@ -3,6 +3,8 @@ import numpy as np
 from manimlib import *
 from typing import Any, Dict, Tuple, List, Optional
 
+from numpy.lib.function_base import iterable
+
 ### Util functions and classes BEGIN ##
 
 # Content of my_util_classes/geometry_structure.py
@@ -16,11 +18,22 @@ def cmp_unordered(tuple1, tuple2):
             return False
     return True
 
+
 class GeometryStructure(VGroup):
+    __vmobjects_append: List[VMobject] = list()
+    __vmobjects_remove: List[VMobject] = list()
+    __vmobjects_targeted: List[VMobject] = list()
+    __point_dict: Dict[str, np.ndarray]
+    __dots: Dict[str, Dot]
+    __labels: Dict[str, Tex]
+    __lines: Dict[Tuple[str, str], Line]
+
+    label_pos_func = lambda p:UR
+
     def __init__(
                 self, 
                 point_dict: Dict[str, Any], 
-                line_list: List, 
+                conn_list: List, 
                 label_pos_func=lambda p:UR, 
                 color_index: Optional[Dict[str, int]] = None, 
                 **kwargs
@@ -28,22 +41,23 @@ class GeometryStructure(VGroup):
         super().__init__(**kwargs)
 
         self.__point_dict = {name: np.array(vertex) for name, vertex in point_dict.items()}
-        self.__connections = [tuple(conn) for conn in line_list if len(conn) == 2]
-
-        for i in range(len(self.__connections)):
-            if isinstance(self.__connections[i], str):
-                conn_str = self.__connections[i]
-                self.__connections[i] = (conn_str[0], conn_str[1])
-            elif not isinstance(self.__connections[i], tuple):
-                conn = self.__connections[i]
-                self.__connections[i] = tuple(conn)
-
         self.__dots = {name: Dot(vertex) for name, vertex in self.__point_dict.items()}
         self.__labels = {
             name: Tex(name).next_to(dot, label_pos_func(self.__point_dict[name])) 
             for name, dot in self.__dots.items()
         }
-        self.__lines = {(a, b): Line(self.__point_dict[a], self.__point_dict[b]) for a, b in self.__connections}
+
+        connections = list()
+
+        for conn in conn_list:
+            if isinstance(conn, str):
+                connections.append((conn[0], conn[1]))
+            elif isinstance(conn, list):
+                connections.append(tuple(conn[0:2]))
+            elif isinstance(conn, tuple):
+                connections.append(conn)
+
+        self.__lines = {(a, b): Line(self.__point_dict[a], self.__point_dict[b]) for a, b in connections}
 
         self.label_pos_func = label_pos_func
 
@@ -69,56 +83,116 @@ class GeometryStructure(VGroup):
         
         return self
 
-    def generate_delta_objects(self) -> Tuple[List[VMobject], List[VMobject]]:
-        append_list = list()
-        remove_list = list()
+    def animate_changes(self, scene: Scene, **kwargs):
+        list_append, list_remove, list_targeted = self.apply_changes()
 
-        for name, point in self.__point_dict.items():
-            if name not in self.__dots:
-                dot = Dot(point)
-                label = Tex(name).next_to(dot, self.label_pos_func(self.__point_dict[name]))
-                self.__dots[name] = dot
-                self.__labels[name] = label
-                append_list.append(dot)
-                append_list.append(label)
-        for name in self.__dots.keys():
-            if name not in self.__point_dict:
-                remove_list.append(self.__dots.pop(name))
-                remove_list.append(self.__labels.pop(name))
-                for conn in self.__connections:
-                    if name in conn:
-                        self.__connections.remove(conn)
+        animations = list()
+        animations.extend([ShowCreation(x) for x in list_append])
+        animations.extend([Uncreate(x) for x in list_remove])
+        animations.extend([MoveToTarget(x) for x in list_targeted])
 
-        for conn in self.__connections:
-            if conn not in self.__lines:
-                line = Line(self.__point_dict[conn[0]], self.__point_dict[conn[1]])
-                self.__lines[conn] = line
-                append_list.append(line)
-        for conn in self.__lines.keys():
-            if conn not in self.__connections:
-                remove_list.append(self.__lines.pop(conn))
+        scene.play(*animations, **kwargs)
 
-        self.remove(*remove_list)
-        self.add(*append_list)
-        return append_list, remove_list
+    def apply_changes(self) -> Tuple[List[VMobject], List[VMobject], List[VMobject]]:
+        list_append = self.__vmobjects_append
+        list_remove = self.__vmobjects_remove
+        list_targeted = self.__vmobjects_targeted
+        self.__vmobjects_append = list()
+        self.__vmobjects_remove = list()
+        self.__vmobjects_targeted = list()
+
+        self.remove(*list_remove)
+        self.add(*list_append)
+
+        return list_append, list_remove, list_targeted
 
     def add_point(self, name:str, coord):
         co = np.array(coord)
         self.__point_dict[name] = co
+
+        dot = Dot(co)
+        self.__dots[name] = dot
+        self.__vmobjects_append.append(dot)
+
+        label = Tex(name).next_to(dot, self.label_pos_func(co))
+        self.__labels[name] = label
+        self.__vmobjects_append.append(label)
         return self
         
     def connect(self, *conn_list):
-        new_connections = [tuple(conn) for conn in conn_list if len(conn) == 2]
+        new_connections = list()
 
-        for i in range(len(new_connections)):
-            if isinstance(new_connections[i], str):
-                conn_str = new_connections[i]
-                new_connections[i] = (conn_str[0], conn_str[1])
-            elif not isinstance(new_connections[i], tuple):
-                conn = new_connections[i]
-                new_connections[i] = tuple(conn)
+        for conn in conn_list:
+            if isinstance(conn, str):
+                new_connections.append((conn[0], conn[1]))
+            elif isinstance(conn, list):
+                new_connections.append(tuple(conn[0:2]))
+            elif isinstance(conn, tuple):
+                new_connections.append(conn)
 
-        self.__connections.extend(new_connections)
+        #self.__connections.extend(new_connections)
+
+        for conn in new_connections:
+            line = Line(self.__point_dict[conn[0]], self.__point_dict[conn[1]])
+            self.__lines[conn] = line
+            self.__vmobjects_append.append(line)
+
+    def disconnect(self, *disconn_list):
+        normalized_list = list()
+
+        for conn in disconn_list:
+            if isinstance(conn, str):
+                normalized_list.append((conn[0], conn[1]))
+            elif isinstance(conn, list):
+                normalized_list.append(tuple(conn[0:2]))
+            elif isinstance(conn, tuple):
+                normalized_list.append(conn)
+
+        keys_to_remove = list()
+        for conn_tbd in normalized_list:
+            for conn in self.__lines.keys():
+                if cmp_unordered(conn_tbd, conn):
+                    keys_to_remove.append(conn)
+        for key in keys_to_remove:
+            self.__vmobjects_remove.append(self.__lines.pop(key))
+
+    def remove_point(self, name:str):
+        self.__point_dict.pop(name)
+        self.__vmobjects_remove.append(self.__dots.pop(name))
+        
+        keys_to_remove = list()
+        for conn in self.__lines.keys():
+            if name in conn:
+                keys_to_remove.append(conn)
+        for key in keys_to_remove:
+            self.__vmobjects_remove.append(self.__lines.pop(key))
+
+        self.__vmobjects_remove.append(self.__labels.pop(name))
+
+    def move_dot(self, name, point):
+        if name in self.__point_dict:
+            if not isinstance(point, np.ndarray):
+                point = np.array(point)
+
+            dot = self.__dots[name]
+            label = self.__labels[name]
+            
+            if dot not in self.__vmobjects_targeted:
+                dot.generate_target()
+                self.__vmobjects_targeted.append(dot)
+            if label not in self.__vmobjects_targeted:
+                label.generate_target()
+                self.__vmobjects_targeted.append(label)
+            
+            self.__point_dict[name] = point
+            dot.target.move_to(point)
+            label.target.next_to(dot.target, self.label_pos_func(point))
+            
+            for conn, line in self.__lines.items():
+                if name in conn:
+                    if line not in self.__vmobjects_targeted:
+                        self.__vmobjects_targeted.append(line)
+                    line.target = Line(self.__point_dict[conn[0]], self.__point_dict[conn[1]])
 
     def get_dot(self, name):
         return self.__dots[name]
@@ -126,9 +200,13 @@ class GeometryStructure(VGroup):
     def get_label(self, name):
         return self.__labels[name]
 
+    def get_point(self, name):
+        return self.__point_dict[name]
+
     def set_dot_color(self, name, color):
         self.get_dot(name).set_color(color)
         self.get_label(name).set_color(color)
+
 
 class VertexPolygon(GeometryStructure):
     def __init__(self, *vertices, color_index=None, name_index='A', **kwargs):
@@ -147,6 +225,7 @@ class VertexPolygon(GeometryStructure):
             color_dict = {names[i]: color_index[i] for i in range(count)}
 
         self.vertices = np.array(vertices)
+        self.__vertex_names = names
 
         def lpfunc(p):
             delta = p-self.get_grav_center()
@@ -164,16 +243,23 @@ class VertexPolygon(GeometryStructure):
         )
 
     def apply_points_function(self, func, about_point=None, about_edge=ORIGIN, works_on_bounding_box=False):
-        super().apply_points_function(func, about_point, about_edge, works_on_bounding_box)
-
         for vertex in self.vertices:
             if about_point is None:
                 vertex[:] = func(vertex)
             else:
                 vertex[:] = func(vertex - about_point) + about_point
+
+        return super().apply_points_function(func, about_point, about_edge, works_on_bounding_box)
         
     def get_grav_center(self):
         return np.sum(self.vertices, axis=0) / len(self.vertices)
+
+    def move_dot(self, name, point):
+        for i, n in enumerate(self.__vertex_names):
+            if n == name:
+                self.vertices[i] = point
+                break
+        return super().move_dot(name, point)
 
 
 ### Util functions and classes END ##
@@ -181,15 +267,21 @@ class VertexPolygon(GeometryStructure):
 
 class Vec2Cross(Scene):
     def construct(self):
-        tri = VertexPolygon((1.5, 3, 0), (-3, -0.5, 0), (3, -1.5, 0), color_index=[RED, GREEN, BLUE]).center().scale(0.8)
+        self.showTriangle()
+
+    def showTriangle(self):
+        tri = VertexPolygon((1.5, 3, 0), (-3, -0.5, 0), (3, -1.5, 0), color_index=[RED, GREEN, BLUE]).center()
         
         self.play(ShowCreation(tri))
 
         tri.add_point('O', tri.get_grav_center())
         tri.connect('OA', 'OB', 'OC')
-        append_list, remove_list = tri.generate_delta_objects()
-
         tri.set_dot_color('O', PURPLE)
-        show_list = [ShowCreation(x) for x in append_list]
-        self.play(*show_list)
-        pass
+
+        tri.animate_changes(self)
+
+        tri.move_dot('C', [1, 0, 0])
+        tri.move_dot('O', tri.get_grav_center())
+
+        tri.animate_changes(self)
+        
